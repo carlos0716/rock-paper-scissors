fa0c732ea185c096ebe2a9882bd2f6252fbc7882
carlos0716 <chulezyc@gmail.com>
2022-07-19 10:35:45 -0600

    we are done  with the second problem, at the begininng we need two variables, one for the player call and other for the computer's.--> the variables are created after the function to store the arguments we need to call the function. our function needs to declare a winner and a loser based on a series of logical conditions. rock beats scissors, scissors beats paper, paper, beats rock variables playerSelection type string init val '' computerSelection type string init val '' output who win and why. input: player with a prompt function and computer with a function called computerPlay ****IN WHICH CASES PLAYER WINS**** Chooses paper and cpu chooses rock. chooses rock cpu chooses scissors. scissors vs paper
    ***IN WHICH CASES CPU WINS***
    player rock cpu paper. player paper cpu scissors. player scissors cpu rock.
    
    *** IN WHICH CASES THERE IS NO WINNER****
    any other combination ends in a tie.
    
    I did not had problem writing the logic conditions to generate the results, but I had problem with the variables creation and the fact that the const to contain the choices had the same name as the arguments kind of confuse me too.

--- script.js
+++ script.js
@@ -10,44 +10,53 @@
 
 // let randomItem = myArray[Math.floor(Math.random()* myArray.length)];
 // console.log(randomItem);
-let computerChoice = ['rock', 'paper', 'scissors']
-function computerPlay() {
-    let computerAnswer = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-    return alert(computerAnswer);
-}
+
 
 // let computerSelection = computerPlay();
 // let playerSelection = prompt("Do you want to play Rock, Paper or Scissors?");
 
+
 function playRound(playerSelection, computerSelection) {
-    
     
     if(playerSelection === "rock" && computerSelection === "paper") {
         return alert(`You lose ${computerSelection} beats ${playerSelection}`);
     }
+    
     if(playerSelection === "paper" && computerSelection === "scissors") {
         return alert(`You lose ${computerSelection} beats ${playerSelection}`);
     }
+    
     if(playerSelection === "scissors" && computerSelection === "rock") {
         return alert(`You lose ${computerSelection} beats ${playerSelection}`);
     }
-    if(playerSelection === computerSelection) {
-        return alert(`That was a tie!!`)
-    }
+    
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        return alert("try again")
+        return alert(`You win ${playerSelection} beats ${computerSelection}`);
+    }
+    
+    if(playerSelection === "paper" && computerSelection === "rock") {
+        return alert(`You win ${playerSelection} beats ${computerSelection}`);
+    }
+    
+    if(playerSelection === "scissors" && computerSelection === "paper") {
+        return alert(`You win ${playerSelection} beats ${computerSelection}`);
     }
     
     else {
-        return alert("Wrong data")
+        alert("Try again");
     }
 }   
 
-// const playerSelection = prompt("Do you want to play Rock, Paper or Scissors?");
-// const computerSelection = computerPlay()
-playRound("paper", "scissors");
+function computerPlay() { // I had this code block at the beginning of the file, once I move it after the main function the program worked as expected.
+    let computerChoice = ['rock', 'paper', 'scissors']
+    let computerAnswer = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    return computerAnswer;
+}
 
-
+const playerSelection = prompt("Enter choice:");
+const playerSelectionInsensitive = playerSelection.toLowerCase();
+const computerSelection = computerPlay()
+playRound(playerSelectionInsensitive, computerSelection);
 
 
 

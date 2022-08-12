4d52eeeac7275c6ba40179b7cc8632bcf0798eba
carlos0716 <chulezyc@gmail.com>
2022-08-12 09:14:47 -0600

    I add parentheses to the getComputerChoice function that acts as an argument for playRound function. This solved the problem with the loop, and now it runs just five games. Now is pending how to integrate the score.

--- script.js
+++ script.js
@@ -15,48 +15,124 @@
 // let computerSelection = computerPlay();
 // let playerSelection = prompt("Do you want to play Rock, Paper or Scissors?");
 
+function game() {
+    
+     for (let i = 0; i < 5; i++) {   
+        let playerSelection = prompt('Enter selection');
+    playRound(playerSelection, getComputerChoice());
+     }
+}
 
 function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        return alert(`It's a Draw`)
+    }
     
-    if(playerSelection === "rock" && computerSelection === "paper") {
+    else if(playerSelection === "rock" && computerSelection === "paper") {
         return alert(`You lose ${computerSelection} beats ${playerSelection}`);
     }
     
-    if(playerSelection === "paper" && computerSelection === "scissors") {
+    else if(playerSelection === "paper" && computerSelection === "scissors") {
         return alert(`You lose ${computerSelection} beats ${playerSelection}`);
     }
     
-    if(playerSelection === "scissors" && computerSelection === "rock") {
+    else if(playerSelection === "scissors" && computerSelection === "rock") {
         return alert(`You lose ${computerSelection} beats ${playerSelection}`);
     }
     
-    if (playerSelection === "rock" && computerSelection === "scissors") {
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
         return alert(`You win ${playerSelection} beats ${computerSelection}`);
     }
     
-    if(playerSelection === "paper" && computerSelection === "rock") {
+    else if(playerSelection === "paper" && computerSelection === "rock") {
         return alert(`You win ${playerSelection} beats ${computerSelection}`);
     }
     
-    if(playerSelection === "scissors" && computerSelection === "paper") {
+    else if(playerSelection === "scissors" && computerSelection === "paper") {
         return alert(`You win ${playerSelection} beats ${computerSelection}`);
     }
     
     else {
         alert("Try again");
-    }
+    } 
 }   
 
-function computerPlay() { // I had this code block at the beginning of the file, once I move it after the main function the program worked as expected.
-    let computerChoice = ['rock', 'paper', 'scissors']
+function getComputerChoice() { // I had this code block at the beginning of the file, once I move it after the main function the program worked as expected.
+    let computerChoice = ['rock', 'paper', 'scissors'];
     let computerAnswer = computerChoice[Math.floor(Math.random() * computerChoice.length)];
     return computerAnswer;
 }
 
-const playerSelection = prompt("Enter choice:");
-const playerSelectionInsensitive = playerSelection.toLowerCase();
-const computerSelection = computerPlay()
-playRound(playerSelectionInsensitive, computerSelection);
+// const playerSelection = prompt("Enter choice:");
+// const playerSelectionInsensitive = playerSelection.toLowerCase();
+// const computerSelection = getComputerChoice();  
+// playRound(playerSelectionInsensitive, computerSelection);
 
 
+// function getComputerChoice() {    
+//     let computerChoice = ['rock', 'paper', 'scissors'];
+//     let computerAnswer = computerChoice[Math.floor(Math.random() * computerChoice.length)];  
+//     return computerAnswer;
+// }
 
+// let example = Math.floor(Math.random()*10+1);
+// console.log(example);
+
+// let exampleTwo = Math.floor(Math.random()  * 3 + 1);
+// console.log(exampleTwo);
+
+// function playRound(playerSelection, computerSelection) {
+    
+//     if(playerSelection === "rock" && computerSelection === "paper") {
+//         return alert(`You lose ${computerSelection} beats ${playerSelection}`);
+//     }
+    
+//     if(playerSelection === "paper" && computerSelection === "scissors") {
+//         return alert(`You lose ${computerSelection} beats ${playerSelection}`);
+//     }
+    
+//     if(playerSelection === "scissors" && computerSelection === "rock") {
+//         return alert(`You lose ${computerSelection} beats ${playerSelection}`);
+//     }
+    
+//     if (playerSelection === "rock" && computerSelection === "scissors") {
+//         return alert(`You win ${playerSelection} beats ${computerSelection}`);
+//     }
+    
+//     if(playerSelection === "paper" && computerSelection === "rock") {
+//         return alert(`You win ${playerSelection} beats ${computerSelection}`);
+//     }
+    
+//     if(playerSelection === "scissors" && computerSelection === "paper") {
+//         return alert(`You win ${playerSelection} beats ${computerSelection}`);
+//     }
+    
+//     else {
+//         alert("Try again");
+//     }
+// }   
+
+// function getComputerChoice() { // I had this code block at the beginning of the file, once I move it after the main function the program worked as expected.
+//     let computerChoice = ['rock', 'paper', 'scissors']
+//     let computerAnswer = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+//     return computerAnswer;
+// }
+
+// const playerSelection = prompt("Enter choice:");
+// const playerSelectionInsensitive = playerSelection.toLowerCase();
+// const computerSelection = getComputerChoice();  
+// playRound(playerSelectionInsensitive, computerSelection);
+
+
+// function getComputerChoice() {    
+//     let computerChoice = ['rock', 'paper', 'scissors'];
+//     let computerAnswer = computerChoice[Math.floor(Math.random() * computerChoice.length)];  
+//     return computerAnswer;
+// }
+
+// let example = Math.floor(Math.random()*10+1);
+// console.log(example);
+
+// let exampleTwo = Math.floor(Math.random()  * 3 + 1);
+// console.log(exampleTwo);
